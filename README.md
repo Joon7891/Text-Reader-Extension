8407e28d23eddc4e6ba25100e29b37d91491cdd0
Joon Song <joon7891@gmail.com>
2020-06-12 13:04:56 -0400

    Cleaned up various errors

---
+++ README.md
@@ -0,0 +1,5 @@
+# Text Reader Extension
+
+Lightweight Text-to-Speech Google Chrome extension built using HTML/CSS, JavaScript, jQuery, and BootStrap.
+
+![](images/Image.png)
--- popup.js
+++ popup.js
@@ -1,43 +1,44 @@
-//let voices = []
+let all_voices = []
 
 window.onload = function(){
+    chrome.tts.getVoices(
+        function(voices) {
+            for (let i = 0; i < voices.length; i++){
+                all_voices.push(voices[i]);
+            }
+        }
+    );
+
     $(document).ready(function() {
+        // Updating slider
         const $valueSpan = $('.valueSpan');
         const $value = $('#speed');
         $valueSpan.html('x' + ($value.val() / 100).toFixed(2));
         $value.on('input change', () => {
             $valueSpan.html('x' + ($value.val() / 100).toFixed(2));
         });
+
+        // Updating languages drop down menu
+        chrome.extension.getBackgroundPage().console.log(all_voices.length);
     });
-
-    /*
-    chrome.tts.getVoices(
-        function(voices) {
-            for (let i = 0; i < voices.length; i++){
-                chrome.extension.getBackgroundPage().console.log(voices[i]);
-                voices.push(voices[i]);
-            }
-        }
-    );
-    */
-
-   chrome.extension.getBackgroundPage().console.log('ok');
     
     let speedButton = document.getElementById('speech-button');
-    speedButton.onclick = function read(){
-        chrome.tabs.executeScript({
-            code: 'window.getSelection().toString();'
-        }, 
-        function(selection){
-            const $value = $('#speed');
-    
-            if (selection[0] === ''){
-                chrome.tts.speak('Please Select Text');
-            }
-            else {
-                if ($value.val() == 0) return;
-                chrome.tts.speak(selection[0], {'lang': 'en-US', 'rate': $value.val() / 100});
-            }
-        });
+    if (speedButton !== null){
+        speedButton.onclick = function read(){
+            chrome.tabs.executeScript({
+                code: 'window.getSelection().toString();'
+            }, 
+            function(selection){
+                const $value = $('#speed');
+        
+                if (selection[0] === ''){
+                    chrome.tts.speak('Please Select Text');
+                }
+                else {
+                    if ($value.val() == 0) return;
+                    chrome.tts.speak(selection[0], {'lang': 'en-US', 'rate': $value.val() / 100});
+                }
+            });
+        }
     }
 }
